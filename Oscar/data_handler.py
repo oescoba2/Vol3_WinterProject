a1e6573e152ce395dd6723e67cdd632d7aabeecb
OscarJEscobar <oescoba2@byu.edu>
2025-03-27 16:44:10 -0600

    Updated data_handler

--- Oscar/data_handler.py
+++ Oscar/data_handler.py
@@ -132,25 +132,22 @@ class PhilDataGetter():
 
         return df
     
-def merger(data_paths:list[str, ...], dfs_freqs:list[str, ...], dfs_index_cols:list[int, ...],
-          cols_to_drop:dict[int:list[str]|None]=None, date_range:tuple[str, str]=('2007-01-01', '2022-12-01'), 
-          write_to_csv:bool=False, path_to_file:str='./Data/PHL_data.csv') -> pd.DataFrame:
-    """Merge any given time series (stored in csv) pandas dataframes along a given date range into one dataframe
+def merger(dataframes:list[pd.DataFrame], cols_to_drop:dict[int:list[str]|None]=None, 
+           date_range:tuple[str, str]=('2007-01-01', '2022-12-01'), 
+            write_to_csv:bool=False, path_to_file:str='./Data/PHL_data.csv') -> pd.DataFrame:
+    """Merge any given time series data (in pd.DataFrame)  along a given date range into one dataframe
     whose time series frequency is 'MS' (i.e. month start).
     
     Parameters:
-        - data_paths (list) : a list of strings comprised of the paths to the csv files containing the dataframes.
-        - df_freqs (list) : a list of strings denoting the frequency of the recorded time series found in the csv
-                            file. The frequency must be any of the supported types: 'YS, 'MS', or 'D'.
-        - dfs_index_cols (list) : a list comprised of integers where each k-integer denotes the index column of 
-                                  the data found in data_path[k].
-        - cols_to_drop (dict or None) : a dictionary comprising of integer keys and lists of string elements (or None) 
-                                        as the values, where key k denotes data_path[k] and the value is a list of
-                                        labels of the columns that must be dropped from data_path[k] along axis 1. If
-                                        the value of data_path[k] is None, no columns are dropped. This parameter is
-                                        defaulted to None which denotes no columns are dropped from any of the datasets.
-        - date_range (tuple) : a tuple comprised of two strings where the first denotes the starting date and the last
-                               the ending date with which to index the merged dataframe with. Defaulted to 
+        - dataframes (list) : the dataframes to merge.
+        - cols_to_drop (dict or None) : a dictionary comprising of integer keys and lists of string 
+                                        elements (or None) as the values, where key k denotes data_path[k] 
+                                        and the value is a list of labels of the columns that must be dropped 
+                                        from data_path[k] along axis 1. If the value of data_path[k] is None, 
+                                        no columns are dropped. This parameter is defaulted to None which 
+                                        denotes no columns are dropped from any of the datasets.
+        - date_range (tuple) : a tuple comprised of two strings where the first denotes the starting date and 
+                               the last the ending date with which to index the merged dataframe with. Defaulted to 
                                ('2007-01-01', '2022-12-01').
         - write_to_csv (bool) : whether to write the created merged dataframe to a csv file. Defaulted to False.
         - path_to_file (str) : a string denoting the path to use to write and store the merged dataframe. Defaulted
@@ -162,17 +159,15 @@ def merger(data_paths:list[str, ...], dfs_freqs:list[str, ...], dfs_index_cols:l
 
     time_freq = 'MS'                                                                 # Monthly Start
     dates = pd.date_range(start=date_range[0], end=date_range[1], freq=time_freq)
-    dataframes = []
+    dfs = []
 
     if cols_to_drop is None:
-        cols_to_drop = {j:None for j in range(len(data_paths))}
+        cols_to_drop = {j:None for j in range(len(dataframes))}
 
-    for i, (data_path, data_freq, index_col) in enumerate(zip(data_paths, dfs_freqs, dfs_index_cols)):
+    for i, df in enumerate(dataframes):
 
-
-        df = pd.read_csv(data_path, index_col=index_col)
         df.index = pd.to_datetime(df.index)
-        df = df.asfreq(data_freq)
+        data_freq = df.index.freq
 
         if cols_to_drop[i] is not None:
             df = df.drop(labels=cols_to_drop[i], axis=1)
@@ -188,9 +183,9 @@ def merger(data_paths:list[str, ...], dfs_freqs:list[str, ...], dfs_index_cols:l
                 df = (df.resample(time_freq)).interpolate('linear')
 
         df = df.reindex(dates)
-        dataframes += [df]
+        dfs += [df]
 
-    merged = pd.concat(dataframes, axis=1)
+    merged = pd.concat(dfs, axis=1)
 
     if write_to_csv:
         print(f'Exporting merged dataframe to path: {path_to_file}')
