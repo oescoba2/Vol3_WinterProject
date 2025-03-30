1d6ab9382bf722fc468373d8d305da6780f0511f
OscarJEscobar <oescoba2@byu.edu>
2025-03-29 23:54:38 -0600

    Added my workbook and data

--- Oscar/data_handler.py
+++ Oscar/data_handler.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 class PhilDataGetter():
-    """Class to extract region, market, and product data for the Philippines.
+    """Class to extract data for the Philippines.
 
     Attributes:
         - data (pd.DataFrame) : the dataframe containing all the data for PHL.
@@ -11,31 +11,37 @@ class PhilDataGetter():
         - get_market_data() : method to get data for markets
     """
 
-    def __init__(self, market_csv_path:str) -> None:
+    def __init__(self, fpi_csv_path:str, er_csv_path:str) -> None:
         """Constructor of class.
         
         Parameters:
-            - market_csv_path (str) : the path to the csv file containing all the price index data 
+            - fpi_csv_path (str) : the path to the csv file containing all the price index data 
             for the several regions, markets, and products found in the Philippines.
+            - er_csv_path (str) : the path to the csv file containing all the exchange rate data 
+            for the several regions and markets found in the Philippines.
             
         Returns
             - None
         """
 
-        self.data = pd.read_csv(market_csv_path)
+        self.fpi_data = pd.read_csv(fpi_csv_path)
+        self.er_data = pd.read_csv(er_csv_path)
 
-    def get_region_data(self, target_regions:str, target_markets:str|list[str], target_products:str|list[str],
-                        write_to_csv:bool=False, path_to_file:str='./Data/region_data.csv') -> pd.DataFrame:
+
+    def get_region_data(self, data_type:str, target_regions:str, target_markets:str|list[str], target_products:str|list[str]=None,
+                        write_to_csv:bool=False, path_to_file:str='./Data/region_data.csv') -> pd.DataFrame|None:
         """Grab data for local markets for the specified products (under specified regions) that are 
         available in those regions' markets.
         
         Parameters: 
-            - target_regions (str or list): 
+            - data_type (str) : the type of data to get from the provided csv files. It can be 'er' 
+                                for exchange rate data OR 'fpi' for food price index data.
+            - target_regions (str or list): regions to extract data for.
             - target_markets (str or list) : a string or list of strings specifying the market or 
-                                             markets from which to extract product information.
+                                             markets to extract data for.
             - target_products (str or list) : a string or list of strings specifying the product
                                               or products to get data for, under the specified 
-                                              market(s).
+                                              market(s) and region(s). Defaulted to None.
             - write_to_csv (bool) : whether to write the created dataframe to a csv file. Defaulted to 
                                    False.
             - path_to_file (str) : a string denoting the path to use to write and store the create 
@@ -47,44 +53,66 @@ class PhilDataGetter():
                              index by DateTimeIndex.
         """
 
-        if isinstance(target_regions, str):
-            target_regions = [target_regions]
-        if isinstance(target_markets, str):
-            target_markets = [target_markets]
-        if isinstance(target_products, str):
-            target_products = [target_products]
+        if (data_type.lower() != 'er') and (data_type.lower() != 'fpi'):
+            raise ValueError("Data type to extract must be either 'fpi' for food price indices OR 'er' for exchange rates.")
 
-        num_regions = len(target_regions)
-        num_markets = len(target_markets)
-        num_products = len(target_products)
+        if data_type == 'er':
+            
+            if isinstance(target_markets, str):
+                target_markets = [target_markets]
+            if isinstance(target_regions, str):
+                target_regions = [target_regions]
+            num_regions = len(target_regions)
+            num_markets = len(target_markets)
 
-        df = self.data[((self.data['Region'].isint(target_regions)) & (self.data['Market']).isin(target_markets)) & ((self.data['Product']).isin(target_products))]
-        df.index = pd.to_datetime(df['Date'])
-        df = df.asfreq('MS')
+            df = self.er_data[self.er_data['Market'].isin(target_markets) & (self.er_data['Region'].isin(target_regions))]
+            df.index = pd.to_datetime(df['Date'])
+            df = df.asfreq('MS')
+            df = df.drop(labels=['Date'], axis=1)
 
-        if (num_regions == 1) and (num_markets == 1) and (num_products == 1):
-            df = df.drop(labels=['Date', 'Product', 'Region', 'Market'], axis=1)
+            if num_markets == 1:
+                df = df.drop(labels=['Market'], axis=1)
 
-        elif (num_regions == 1) and (num_markets == 1) and (num_products != 1):
-            df = df.drop(labels=['Date', 'Region', 'Market'], axis=1)
+        else:
 
-        elif (num_regions == 1) and (num_markets != 1) and (num_products == 1):
-            df = df.drop(labels=['Date', 'Region', 'Product'], axis=1)   
+            if isinstance(target_regions, str):
+                target_regions = [target_regions]
+            if isinstance(target_markets, str):
+                target_markets = [target_markets]
+            if isinstance(target_products, str):
+                target_products = [target_products]
 
-        elif (num_regions != 1) and (num_markets == 1) and (num_products == 1):
-            df = df.drop(labels=['Date', 'Market', 'Product'], axis=1)   
+            num_regions = len(target_regions)
+            num_markets = len(target_markets)
+            num_products = len(target_products)
 
-        elif (num_regions == 1) and (num_markets != 1) and (num_products != 1):
-            df = df.drop(labels=['Date', 'Region'], axis=1)
+            df = self.dpi_data[((self.fpi_data['Region'].isint(target_regions)) & (self.fpi_data['Market']).isin(target_markets)) & ((self.fpi_data['Product']).isin(target_products))]
+            df.index = pd.to_datetime(df['Date'])
+            df = df.asfreq('MS')
 
-        elif (num_regions != 1) and (num_markets != 1) and (num_products == 1):
-            df = df.drop(labels=['Date', 'Product'], axis=1)
+            if (num_regions == 1) and (num_markets == 1) and (num_products == 1):
+                df = df.drop(labels=['Date', 'Product', 'Region', 'Market'], axis=1)
 
-        elif (num_regions != 1) and (num_markets == 1) and (num_products != 1):
-            df = df.drop(labels=['Date', 'Market'], axis=1)
+            elif (num_regions == 1) and (num_markets == 1) and (num_products != 1):
+                df = df.drop(labels=['Date', 'Region', 'Market'], axis=1)
 
-        elif (num_regions != 1) and (num_markets != 1) and (num_products != 1):
-            df = df.drop(labels=['Date'], axis=1)  
+            elif (num_regions == 1) and (num_markets != 1) and (num_products == 1):
+                df = df.drop(labels=['Date', 'Region', 'Product'], axis=1)   
+
+            elif (num_regions != 1) and (num_markets == 1) and (num_products == 1):
+                df = df.drop(labels=['Date', 'Market', 'Product'], axis=1)   
+
+            elif (num_regions == 1) and (num_markets != 1) and (num_products != 1):
+                df = df.drop(labels=['Date', 'Region'], axis=1)
+
+            elif (num_regions != 1) and (num_markets != 1) and (num_products == 1):
+                df = df.drop(labels=['Date', 'Product'], axis=1)
+
+            elif (num_regions != 1) and (num_markets == 1) and (num_products != 1):
+                df = df.drop(labels=['Date', 'Market'], axis=1)
+
+            elif (num_regions != 1) and (num_markets != 1) and (num_products != 1):
+                df = df.drop(labels=['Date'], axis=1)  
 
         if write_to_csv:
             print(f'Exporting merged dataframe to path: {path_to_file}')
@@ -92,16 +120,18 @@ class PhilDataGetter():
 
         return df
 
-    def get_market_data(self, target_markets:str|list[str], target_products:str|list[str],
+    def get_market_data(self, data_type:str, target_markets:str|list[str], target_products:str|list[str]=None,
                         write_to_csv:bool=False, path_to_file:str="./Data/market_data.csv") -> pd.DataFrame:
         """Grab data for local markets for the specified products that are available in those markets.
         
         Parameters: 
+            - data_type (str) : the type of data to get from the provided csv files. It can be 'er' 
+                                for exchange rate data OR 'fpi' for food price index data.
             - target_markets (str or list) : a string or list of strings specifying the market or 
-                                             markets from which to extract product information.
+                                             markets to extract data for.
             - target_products (str or list) : a string or list of strings specifying the product
                                               or products to get data for, under the specified 
-                                              market(s).
+                                              market(s). Defaulted to None.
             - write_to_csv (bool) : whether to write the created dataframe to a csv file. Defaulted to 
                                    False.
             - path_to_file (str) : a string denoting the path to use to write and store the create 
@@ -112,27 +142,51 @@ class PhilDataGetter():
                              index by DateTimeIndex.
         """
 
-        if isinstance(target_markets, str):
-            target_markets = [target_markets]
-        if isinstance(target_products, str):
-            target_products = [target_products]
 
-        num_markets = len(target_markets)
-        num_products = len(target_products)
-        df = self.data[((self.data['Market']).isin(target_markets)) & ((self.data['Product']).isin(target_products))]
-        df.index = pd.to_datetime(df['Date'])
-        df = df.asfreq('MS')
+        if (data_type.lower() != 'er') and (data_type.lower() != 'fpi'):
+            raise ValueError("Data type to extract must be either 'fpi' for food price indices OR 'er' for exchange rates.")
 
-        # One product for one market
-        if (num_markets == 1) and (num_products == 1):
-            df = df.drop(labels=['Date', 'Product', 'Region', 'Market'], axis=1)
+        if data_type == 'er':
 
-        # Several products for one market
-        elif (num_markets == 1) and (num_products != 1):
-            df = df.drop(labels=['Date', 'Region', 'Market'], axis=1)
+            if isinstance(target_markets, str):
+                target_markets = [target_markets]
+            num_markets = len(target_markets)
 
-        elif (num_markets != 1) and (num_products != 1):
-            df = df.drop(labels=['Date', 'Region'], axis=1)
+            df = self.er_data[self.er_data['Market'].isin(target_markets)]
+            df.index = pd.to_datetime(df['Date'])
+            df = df.asfreq('MS')
+            df = df.drop(labels=['Region', 'Date'], axis=1)
+
+            if num_markets == 1:
+                df = df.drop(labels=['Market'], axis=1)
+
+        else:
+
+            if isinstance(target_markets, str):
+                target_markets = [target_markets]
+
+            if target_products is None:
+                raise ValueError("For fpi, target_products must be specified")
+            else:
+                if isinstance(target_products, str):
+                    target_products = [target_products]
+
+            num_markets = len(target_markets)
+            num_products = len(target_products)
+            df = self.fpi_data[((self.fpi_data['Market']).isin(target_markets)) & ((self.fpi_data['Product']).isin(target_products))]
+            df.index = pd.to_datetime(df['Date'])
+            df = df.asfreq('MS')
+
+            # One product for one market
+            if (num_markets == 1) and (num_products == 1):
+                df = df.drop(labels=['Date', 'Product', 'Region', 'Market'], axis=1)
+
+            # Several products for one market
+            elif (num_markets == 1) and (num_products != 1):
+                df = df.drop(labels=['Date', 'Region', 'Market'], axis=1)
+
+            elif (num_markets != 1) and (num_products != 1):
+                df = df.drop(labels=['Date', 'Region'], axis=1)
 
         if write_to_csv:
             print(f'Exporting merged dataframe to path: {path_to_file}')
@@ -140,14 +194,21 @@ class PhilDataGetter():
 
         return df
     
-def merger(dfs:list[pd.DataFrame], date_range:tuple[str, str]=('2007-01-01', '2022-12-01'),
+def merger(dfs:list[pd.DataFrame], date_range:tuple[str, str]=('2007-01-01', '2022-12-01'), along_axis:int=1,
            cols_to_drop:dict[int:list[str]|None]=None, cols_to_rename:dict[int:dict[str:str]|None]=None, 
-            write_to_csv:bool=False, path_to_file:str='./Data/PHL_data.csv') -> pd.DataFrame:
+           write_to_csv:bool=False, path_to_file:str='./Data/PHL_data.csv') -> pd.DataFrame:
     """Merge any given time series data (in pd.DataFrame)  along a given date range into one dataframe
     whose time series frequency is 'MS' (i.e. month start).
     
     Parameters:
         - dfs (list) : the dataframes to merge.
+        - date_range (tuple) : a tuple comprised of two strings where the first denotes the starting date and 
+                        the last the ending date with which to index the merged dataframe with. Defaulted to 
+                        ('2007-01-01', '2022-12-01').
+        - along_axis (int) : an int denoting the axis on which to merge the given dfs. Defaulted to 1 for column
+                             wise merging. NOTE that in order for merging to take place along axis 0, each df in
+                             dfs must have the same column names; else, merging will be along axis 1 and columns
+                             may contain NaNs.
         - cols_to_drop (dict or None) : a dictionary comprising of integer keys and lists of string 
                                         elements (or None) as the values, where key k denotes dfs[k] 
                                         and the value is a list of labels of the columns that must be dropped 
@@ -159,15 +220,12 @@ def merger(dfs:list[pd.DataFrame], date_range:tuple[str, str]=('2007-01-01', '20
                                           names of the columns that needs to be renamed and the values are the new names
                                           of those columns. If the value of dfs[k] is None, no columns are renamed.
                                           This parameter is defaulted to None.
-        - date_range (tuple) : a tuple comprised of two strings where the first denotes the starting date and 
-                               the last the ending date with which to index the merged dataframe with. Defaulted to 
-                               ('2007-01-01', '2022-12-01').
         - write_to_csv (bool) : whether to write the created merged dataframe to a csv file. Defaulted to False.
         - path_to_file (str) : a string denoting the path to use to write and store the merged dataframe. Defaulted
                                to './Data/PHL_data.csv'.
 
     Returns:
-        - (pd.DataFrame) : a dataframe containing merging all the specified data_paths.
+        - (pd.DataFrame) : a dataframe merging all the given dfs.
     """
 
     time_freq = 'MS'                                                                 # Monthly Start
@@ -207,10 +265,11 @@ def merger(dfs:list[pd.DataFrame], date_range:tuple[str, str]=('2007-01-01', '20
             elif data_freq == 'YS':
                 df = (df.resample(time_freq)).interpolate('linear')
 
-        df = df.reindex(dates)
+        if along_axis == 1:
+            df = df.reindex(dates)
         dataframes += [df]
 
-    merged = pd.concat(dataframes, axis=1)
+    merged = pd.concat(dataframes, axis=along_axis)
 
     if write_to_csv:
         print(f'Exporting merged dataframe to path: {path_to_file}')
